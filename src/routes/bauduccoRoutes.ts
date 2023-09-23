a0818f43ef33362041dc75cd2c410d99f43d41a3
Eduardo Veras <126736702+duduveras@users.noreply.github.com>
2023-09-23 00:01:02 -0300

    criação do read da api

--- src/routes/bauduccoRoutes.ts
+++ src/routes/bauduccoRoutes.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import { addProdutoController, getAllProdutoController } from "../controllers/bauduccoController";
+import { addProdutoController, getAllProdutoController, listarSaboresDeBiscoitos } from "../controllers/bauduccoController";
 
 const router = express.Router();
 
 router.get('/', getAllProdutoController);
 router.post('/', addProdutoController);
+router.get('/', listarSaboresDeBiscoitos);
 
 export default router;
 
